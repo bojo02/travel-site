6ae49a7ee7822d5eec42d60ba4d1be82bf81b0c2
Bojidar <bojo1810@gmail.com>
2023-10-31 23:06:58 +0200

    Completed the link active in yellow color and smooth transition between the sections when a link is clicked

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,6 +2,7 @@ import "../styles/styles.css";
 
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
+import StickyHeader from "./modules/StickyHeader";
 
 let mobileMenu = new MobileMenu();
 new RevealOnScroll(
@@ -9,6 +10,8 @@ new RevealOnScroll(
   75
 );
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 60);
+
+let stickyHeader = new StickyHeader();
 
 if (module.hot) {
   module.hot.accept();
